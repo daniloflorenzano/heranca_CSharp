d6ae2ca263e2ca0406cfe598b404e3277791fac0
Danilo Florenzano <daniloflorenzano1@outlook.com>
2022-06-27 22:03:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Projeto.Entities;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            // Upcasting
+            // uma variavel do tipo Super Classe pode receber qualquer Sub Classe
+
+            Account acc1 = bacc;
+        }
+    }
+}
